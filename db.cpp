c23472b1039a730ea06a0d822e0c36517092673d
Kit <74026992+aouwt@users.noreply.github.com>
2021-12-29 10:15:03 -0500

    .

--- db.cpp
+++ db.cpp
@@ -9,32 +9,47 @@ err_t db::init (void) {
 
 err_t db::get_post (postid_t id, post* post) {
 	sqlite3_stmt* stmt;
-	
-	if (sqlite3_prepare_v2 (db, "SELECT * FROM posts WHERE id = ?;", -1, &stmt, NULL))
+
+	if (sqlite3_prepare_v2 (db, "SELECT VALUES (author_id, date, text, body) FROM posts WHERE id = ?;", -1, &stmt, NULL))
 		return 1;
-	
+
 	if (sqlite3_bind_int (stmt, 1, id))
 		return 2;
-	
-	
-	{	int res;
-loop: // yes i know this can be a loop but i dont wanna make it one, okay
-		res = sqlite3_step (stmt);
-		switch (res) {
-			case SQLITE_ROW:
-				post -> id = id;
-				post -> author_id = sqlite3_column_int (stmt, 0);
-				post -> date = sqlite3_column_int64 (stmt, 1);
-				post -> text = sqlite3_column_text (stmt, 2);
-				post -> body = sqlite3_column_text (stmt, 3);
-				
-				sqlite3_finalize (stmt);
-				
-				return 0;
-				
-			case SQLITE_DONE: break;
-			default: return 3;
-		}
-	}
-	
-	
+
+
+	if (sqlite3_step (stmt) != SQLITE_ROW)
+		return 3;
+
+	post -> id = id;
+	post -> author_id = sqlite3_column_int (stmt, 0);
+	post -> date = sqlite3_column_int64 (stmt, 1);
+	post -> text = sqlite3_column_text (stmt, 2);
+	post -> body = sqlite3_column_text (stmt, 3);
+
+	sqlite3_finalize (stmt);
+
+	return 0;
+}
+
+
+err_t db::get_user (userid_t id, user* user) {
+	sqlite3_stmt* stmt;
+
+	if (sqlite3_prepare_v2 (db, "SELECT VALUES (name, fname) FROM users WHERE id = ?", -1, &stmt, NULL))
+		return 1;
+
+	if (sqlite3_bind_int (stmt, 1, id))
+		return 2;
+
+
+	if (sqlite3_step (stmt)	!= SQLITE_ROW)
+		return 3;
+
+	user -> id = id;
+	user -> name = sqlite3_column_text (stmt, 0);
+	user -> fname = sqlite3_column_text (stmt, 1);
+
+	sqlite3_finalize (stmt);
+
+	return 0;
+}
